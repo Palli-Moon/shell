e20a2e9ca9dfba88b0e420a9dcc12f478f9cd8ab
Palli Moon <pallimoon@gmail.com>
2016-08-17 22:27:02 +0000

    Make basic shell

--- shell.c
+++ shell.c
@@ -1,16 +1,113 @@
+#include "shell.h"
+
+#define SH_RL_BUFFSIZE 1024
+#define SH_TOK_BUFFSIZE 64
+#define SH_TOK_DELIM " \t\r\n\a"
+
 void sh_loop(void)
 {
     char *line;
     char **args;
     int status;
 
-    //do {
-    //    printf("> ");
-    //    line = sh_read_line();
-    //    args = sh_split_line(line);
-    //    status = sh_execute(args);
+    do {
+        printf("> ");
+        line = sh_read_line();
+        args = sh_split_line(line);
+        status = sh_execute(args);
 
-    //    free(line);
-    //    free(args);
-    //} while (status);
+        free(line);
+        free(args);
+    } while (status);
+}
+
+char *sh_read_line(void)
+{
+    int buffsize = SH_RL_BUFFSIZE;
+    int position = 0;
+    char *buffer = malloc(sizeof(char) * buffsize);
+
+    if (!buffer) {
+        fprintf(stderr, "sh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    while (1) {
+        c = getchar();
+
+        // Return on EOF or ENTER
+        if (c == EOF || c == '\n') {
+            buffer[position] = '\0';
+            return buffer;
+        } else {
+            buffer[position] = c;
+        }
+
+        position++;
+
+        if (position >= buffsize) {
+            buffsize += SH_RL_BUFFSIZE;
+            buffer = realloc(buffer, buffsize);
+            if (!buffer) {
+                fprintf(stderr, "sh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+    }
+}
+
+char **sh_split_line(char *line)
+{
+    int buffsize = SH_TOK_BUFFSIZE;
+    int position = 0;
+    char **tokens = malloc(buffsize * sizeof(char*));
+    char *token, **tokens_backup;
+
+    if (!tokens) {
+        fprintf(stderr, "sh: allocation error\n");
+        exit(EXIT_FAILURE);
+    }
+
+    token = strtok(line, SH_TOK_DELIM);
+    while (token != NULL) {
+        tokens[position] = token;
+        position++;
+
+        if (position >= buffsize) {
+            buffsize += SH_TOK_BUFFSIZE;
+            tokens = realloc(tokens, buffsize * sizeof(char*));
+            if (!tokens) {
+                fprintf(stderr, "sh: allocation error\n");
+                exit(EXIT_FAILURE);
+            }
+        }
+        
+        token = strtok(NULL, SH_TOK_DELIM);
+    }
+
+    tokens[position] = NULL;
+    return tokens;
+}
+
+int sh_launch(char **args)
+{
+    pid_t pid, wpid;
+    int status;
+
+    pid = fork();
+    if (pid == 0) {
+        // Child
+        if (execvp(args[0], args) == -1) {
+            perror("sh");
+        }
+    } else if (pid < 0) {
+        // Error
+        perror("sh");
+    } else {
+        do {
+            wpid = waitpid(pid, &status, WUNTRACED);
+        } while (!WIFEXITED(status) && !WIFSIGNALED(status));
+    }
+
+    return 1;
 }
--- shell.h
+++ shell.h
@@ -1,1 +1,9 @@
+#ifndef SHELL_H_
+#define SHELL_H_
+
 void sh_loop(void);
+char *sh_read_line(void);
+char **sh_split_line(char *line);
+int sh_launch(char **args);
+
+#endif // SHELL_H_
