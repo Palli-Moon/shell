5f578cec95bced0386f18172e2ffbba5e85f4dc6
Palli Moon <pallimoon@gmail.com>
2016-08-17 21:29:20 +0000

    Add basic files

---
+++ main.c
@@ -0,0 +1,9 @@
+#include "shell.h"
+
+int main(int argc, char **argv)
+{
+    sh_loop();
+
+    //return EXIT_SUCCESS;
+    return 0;
+}
---
+++ shell.c
@@ -0,0 +1,16 @@
+void sh_loop(void)
+{
+    char *line;
+    char **args;
+    int status;
+
+    //do {
+    //    printf("> ");
+    //    line = sh_read_line();
+    //    args = sh_split_line(line);
+    //    status = sh_execute(args);
+
+    //    free(line);
+    //    free(args);
+    //} while (status);
+}
